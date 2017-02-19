d8a22319556c15a6274e68bfb3a696308f68bfec
xconstaud <xconstaud@gmail.com>
2017-02-19 00:50:21 -0600

    Add older mobile compatability with slidebar

--- js/page.js
+++ js/page.js
@@ -3,8 +3,8 @@ var isOpen = true;
 // Function changes values in CSS (Of nav bar and Icon) on click of HTML items, specifically three bar icon I threw in.
 function openNav() {
 	if (isOpen) {
-		document.getElementById("exifSidenav").style.width = "350px";
-		document.getElementById("slidingButton").style.left = "360px";
+		document.getElementById("exifSidenav").style.width = "300px";
+		document.getElementById("slidingButton").style.left = "310px";
 		isOpen = false;
 	}else {
 		document.getElementById("exifSidenav").style.width = "0";
