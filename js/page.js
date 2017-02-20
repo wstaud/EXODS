1437964779fad130ae49f2b6c0c25b13d99bc9c5
xconstaud <xconstaud@gmail.com>
2017-02-19 19:04:48 -0600

    Add basic theme switcher and fixed slide nav

--- js/page.js
+++ js/page.js
@@ -1,16 +1,32 @@
+"use strict";
+
 var isOpen = true;
 
 // Function changes values in CSS (Of nav bar and Icon) on click of HTML items, specifically three bar icon I threw in.
 function openNav() {
-	if (isOpen) {
-		document.getElementById("exifSidenav").style.width = "300px";
-		document.getElementById("slidingButton").style.left = "310px";
-		isOpen = false;
-	}else {
-		document.getElementById("exifSidenav").style.width = "0";
-		document.getElementById("slidingButton").style.left = "10px";
-		isOpen = true;
-	}
-    
+    if (isOpen) {
+        document.getElementById("exifSidenav").style.width = "300px";
+        document.getElementById("slidingButton").style.left = "310px";
+        isOpen = false;
+    }else {
+        document.getElementById("exifSidenav").style.width = "0";
+        document.getElementById("slidingButton").style.left = "10px";
+        isOpen = true;
+    }
 }
+
+
+var isDark = true;
+
+function themeSwitch() {
+    if (isDark) {
+        document.getElementById("background").style.background = "#e8e5e5";
+        isDark = false;
+    }else {
+        document.getElementById("background").style.background = "#131313";
+        isDark = true;
+    }
+}
+
+
 
